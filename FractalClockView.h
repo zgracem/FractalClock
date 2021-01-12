b472857f4814df7a4181e55d6b470d093bb7b311
Z. Grace Moreau <zgracem@users.noreply.github.com>
2021-01-12 15:04:14 -0700

    update copyright in code comments

--- FractalClockView.h
+++ FractalClockView.h
@@ -3,7 +3,9 @@
 //  FractalClock
 //
 //  Created by Rob Mayoff on 5/20/07.
-//  Copyright (c) 2007, Rob Mayoff. Dedicated to the public domain.
+//  Updated for macOS 10.15 by Amy Grace on 1/12/21.
+//  Copyright (c) 2007 Rob Mayoff, 2021 Amy Grace.
+//  Dedicated to the public domain.
 //
 
 #import <ScreenSaver/ScreenSaver.h>
